7f64b33305ccf0a330281be7c9f53bed724805ca
Juniper Lake <jlake@pacificbiosciences.com>
2023-05-10 17:10:29 -0700

    update, format, lint vcf parsing scripts and tests

--- docker/vcfparser/scripts/aggregate_trgt_vcfs.py
+++ docker/vcfparser/scripts/aggregate_trgt_vcfs.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """
-This tool aggregates multiple TRGT output VCFs into a single gzipped TSV and optionally anonymizes the data by:
+This script aggregates multiple TRGT output VCFs into a single 
+gzipped TSV and optionally anonymizes the data by:
 (1) randomizing the order of sample-specific data on a per-variant basis and
 (2) changing the sample names to an arbitrary counter starting with the provided prefix.
 
-It also adds a metadata line to the vcf file to indicate that the file has been aggregated and anonymized.
+It also adds a metadata line to the VCF file to reflect use of this script.
 
-TRGT VCFs should sorted and optionally gzipped.
+TRGT VCFs should be sorted and optionally gzipped.
 """
 
 __version__ = "0.1.0"
@@ -37,55 +38,62 @@ def release(func, *args, exception, handle, message, **kwargs):
         pass
 
 
-def flatten(l):
+def flatten(lst):
     """Flatten a list of lists"""
-    return [item for sublist in l for item in sublist]
+    return [item for sublist in lst for item in sublist]
 
 
 def parse_args(args):
     """Parse command line parameters"""
-    parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument("vcfs", nargs='*', help="list of optionally gzipped TRGT VCF files to be aggregated and aonymized")
-    parser.add_argument("cohort_prefix", help="sample name prefix for header line")
-    parser.add_argument(
-        "--outfile",
-        dest="outfile",
-        help="output file name"
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawTextHelpFormatter
     )
     parser.add_argument(
-        "--loglevel", 
-        dest="loglevel", 
-        choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'], 
-        default='INFO', 
-        help="set the logging level"
+        "vcfs",
+        nargs="*",
+        help="list of optionally gzipped TRGT VCF files to be aggregated and aonymized",
+    )
+    parser.add_argument("cohort_prefix", help="sample name prefix for header line")
+    parser.add_argument("--outfile", dest="outfile", help="output file name")
+    parser.add_argument(
+        "--loglevel",
+        dest="loglevel",
+        choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+        default="INFO",
+        help="set the logging level",
     )
     parser.add_argument(
         "--version",
         action="version",
-        version="%(prog)s (version {version})".format(version=__version__)
+        version="%(prog)s (version {version})".format(version=__version__),
     )
 
     return parser.parse_args()
 
 
-def aggregate_trgt_vcfs(vcf_files:list, anonymize_prefix:str=None, outfile:str=None, meta_string:str='aggregate_trgt_vcfs.py'):
-    """Combines multiple TRGT output VCFs into a single VCF and optionally anonymizes the data"""
-    
+def aggregate_trgt_vcfs(
+    vcf_files: list,
+    anonymize_prefix: str = None,
+    outfile: str = None,
+    meta_string: str = "aggregate_trgt_vcfs.py",
+):
+    """Combine multiple TRGT VCFs into single VCF and optionally anonymize samples"""
+
     # check if outfile exists
     if outfile:
-        if not outfile.endswith('.vcf'):
+        if not outfile.endswith(".vcf"):
             raise ValueError("Output file should end in .vcf")
         if os.path.isfile(outfile):
             raise IOError(f"Output file {outfile} already exists.")
         else:
-            file = open(outfile, 'w')
+            file = open(outfile, "w")
     else:
         logger.info("No output file provided, printing to stdout")
         file = sys.stdout
 
     if len(vcf_files) < 2:
         raise ValueError("Please provide at least two VCF files to aggregate")
-    
+
     # parse all VCFs, create iterators for all vcfs except the first
     vcfs = []
     vcf_iterators = []
@@ -100,69 +108,97 @@ def aggregate_trgt_vcfs(vcf_files:list, anonymize_prefix:str=None, outfile:str=N
             raise ValueError("VCFs have different metadata")
 
     # print metadata and of first vcf
-    vcfs[0].add_meta('commandline', meta_string)
+    vcfs[0].add_meta("commandline", meta_string)
     print(vcfs[0].print_metadata(), file=file)
 
     # print headerline
+    samples = vcfs[0].samples + flatten([vcf.samples for vcf in vcfs[1:]])
     if anonymize_prefix:
-        samples = [f"{anonymize_prefix}_{i}" for i in range(1, len(samples)+1)]
-    else:
-        samples =  vcfs[0].samples + flatten([vcf.samples for vcf in vcfs[1:]])
-
-    headerline = '\t'.join(vcfs[0].header_columns + samples)
+        samples = [f"{anonymize_prefix}_{i}" for i in range(1, len(samples) + 1)]
+    headerline = "\t".join(vcfs[0].header_columns + samples)
     print(headerline, file=file)
 
-    # iterate through first vcf, get corresponding records from other vcfs, aggregate, and print
+    # iterate through first vcf, get corresponding records from other vcfs
     for sample1_record in vcfs[0]:
-    
-        # go to next record for each vcf iterator, if any of them are empty, raise IndexError
-        other_sample_records = [catch(next, vcf_iterator, exception=StopIteration, handle=IndexError, message='VCFs have different numbers of variants') for vcf_iterator in vcf_iterators]
+        # go to next record for each vcf iterator, if any are empty, raise IndexError
+        other_sample_records = [
+            catch(
+                next,
+                vcf_iterator,
+                exception=StopIteration,
+                handle=IndexError,
+                message="VCFs have different numbers of variants",
+            )
+            for vcf_iterator in vcf_iterators
+        ]
 
         # make sure info and format fields are the same for each variant
         for other_sample_record in other_sample_records:
             if sample1_record.info != other_sample_record.info:
-                raise ValueError("VCFs have different INFO fields. Were all VCFs run with the same TRGT parameters and version? Are all VCFs sorted?")
+                raise ValueError(
+                    "VCFs have different INFO fields. Were all VCFs produced with \
+                        the same TRGT parameters and version? Are all VCFs sorted?"
+                )
             if sample1_record.format != other_sample_record.format:
-                raise ValueError("VCFs have different FORMAT fields. Were all VCFs run with the same TRGT parameters and version? Are all VCFs sorted?")
-            
-        alts = sample1_record.alts + flatten([other_sample_record.alts for other_sample_record in other_sample_records])
+                raise ValueError(
+                    "VCFs have different FORMAT fields. Were all VCFs produced with \
+                        the same TRGT parameters and version? Are all VCFs sorted?"
+                )
+
+        alts = sample1_record.alts + flatten(
+            [other_sample_record.alts for other_sample_record in other_sample_records]
+        )
         alts = sorted(list(set(alts)))
 
         # if not all ref calls, update genotypes to reflect full list of alt alleles
         if "." in alts and len(alts) > 1:
             alts.remove(".")
             sample1_record.update_genotypes(ref=sample1_record.ref, alts=alts)
-            [other_sample_record.update_genotypes(ref=sample1_record.ref, alts=alts) for other_sample_record in other_sample_records]
-            sample_data = sample1_record.sample_data + [other_sample_record.sample_data[0] for other_sample_record in other_sample_records]
+            [
+                other_sample_record.update_genotypes(ref=sample1_record.ref, alts=alts)
+                for other_sample_record in other_sample_records
+            ]
+            sample_data = sample1_record.sample_data + [
+                other_sample_record.sample_data[0]
+                for other_sample_record in other_sample_records
+            ]
             if anonymize_prefix:
                 sample_data = random.sample(sample_data, len(sample_data))
 
             # ['CHROM','POS','ID','REF','ALT','QUAL','FILTER','INFO','FORMAT',samples]
-            aggregated_record = '\t'.join([sample1_record.chrom,
-                                sample1_record.pos,
-                                sample1_record.id,
-                                sample1_record.ref,
-                                ','.join(alts),
-                                '.',
-                                '.',
-                                sample1_record.info,
-                                sample1_record.format,
-                                ]
-                                + sample_data)
-            print(aggregated_record, file=file)
+            aggregated_record = [
+                sample1_record.chrom,
+                sample1_record.pos,
+                sample1_record.id,
+                sample1_record.ref,
+                ",".join(alts),
+                ".",
+                ".",
+                sample1_record.info,
+                sample1_record.format,
+            ] + sample_data
+            print("\t".join(aggregated_record), file=file)
 
     # make sure the first vcf doesn't have fewer variants than the others
     for vcf_iterator in vcf_iterators:
-        release(next, vcf_iterator, exception=StopIteration, handle=IndexError, message='VCFs have different numbers of variants')
+        release(
+            next,
+            vcf_iterator,
+            exception=StopIteration,
+            handle=IndexError,
+            message="VCFs have different numbers of variants",
+        )
 
     logger.success("Successfully terminated")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     """This is executed when run from the command line."""
 
     args = parse_args(sys.argv[1:])
 
     logger.configure(handlers=[{"sink": sys.stderr, "level": args.loglevel}])
 
-    aggregate_trgt_vcfs(args.vcfs, args.anonymize_prefix, args.outfile, ' '.join(sys.argv))
+    aggregate_trgt_vcfs(
+        args.vcfs, args.anonymize_prefix, args.outfile, " ".join(sys.argv)
+    )
--- docker/vcfparser/scripts/test_trgt_aggregator.py
+++ docker/vcfparser/scripts/test_trgt_aggregator.py
@@ -9,7 +9,6 @@ __version__ = "0.1.0"
 
 import pytest
 from tempfile import NamedTemporaryFile
-from parser import VCFParser
 import random
 import inspect
 import sys
@@ -19,62 +18,102 @@ from aggregate_trgt_vcfs import aggregate_trgt_vcfs
 def get_vcf_file(vcf_lines):
     """
     Take an iterator with vcf lines and prints them to a temporary file.
-    
+
     Arguments:
         vcf_lines (iterator): An iterator with vcf lines
-    
+
     Returns:
         filename (str): The path to the vcf file
     """
-    vcf_file = NamedTemporaryFile(mode='w+t', delete=False, suffix='.vcf')
+    vcf_file = NamedTemporaryFile(mode="w+t", delete=False, suffix=".vcf")
     vcf_file.writelines(vcf_lines)
     vcf_file.seek(0)
     vcf_file.close()
-    
+
     return vcf_file.name
 
 
-def test_trgt_aggregation():
+def test_trgt_aggregation(capfd):
     vcf_lines1 = [
-        '##fileformat=VCFv4.2\n',
-        '#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\tsample1\n',
-        'chr2\t126162\t.\tREF\tALT1\t0\t.\tTRID=chr2_126161_126197;END=126197;MOTIFS=CTCTCC;STRUC=(CTCTCC)n\tGT:AL:ALLR:SD:MC:MS:AP:AM\t0/1:36,33:35-38,31-33:18,14:6,6:0(0-36),0(0-33):1,1:.,.\n'
+        "##fileformat=VCFv4.2\n",
+        "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\tsample1\n",
+        "chr2\t126162\t.\tREF\tALT1\t0\t.\tTRID=chr2_126161_126197;END=126197;MOTIFS=CTCTCC;STRUC=(CTCTCC)n\t",
+        "GT:AL:ALLR:SD:MC:MS:AP:AM\t0/1:36,33:35-38,31-33:18,14:6,6:0(0-36),0(0-33):1,1:.,.\n",
     ]
 
     vcf_lines2 = [
-        '##fileformat=VCFv4.2\n',
-        '#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\tsample2\n',
-        'chr2\t126162\t.\tREF\tALT2,ALT3\t0\t.\tTRID=chr2_126161_126197;END=126197;MOTIFS=CTCTCC;STRUC=(CTCTCC)n\tGT:AL:ALLR:SD:MC:MS:AP:AM\t1/2:36,33:35-38,31-33:18,14:6,6:0(0-36),0(0-33):1,1:.,.\n'
+        "##fileformat=VCFv4.2\n",
+        "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\tsample2\n",
+        "chr2\t126162\t.\tREF\tALT2,ALT3\t0\t.\tTRID=chr2_126161_126197;END=126197;MOTIFS=CTCTCC;STRUC=(CTCTCC)n\t",
+        "GT:AL:ALLR:SD:MC:MS:AP:AM\t1/2:36,33:35-38,31-33:18,14:6,6:0(0-36),0(0-33):1,1:.,.\n",
     ]
 
     vcf_lines3 = [
-        '##fileformat=VCFv4.2\n',
-        '#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\tsample3\n',
-        'chr2\t126162\t.\tREF\t.\t0\t.\tTRID=chr2_126161_126197;END=126197;MOTIFS=CTCTCC;STRUC=(CTCTCC)n\tGT:AL:ALLR:SD:MC:MS:AP:AM\t0/0:36,33:35-38,31-33:18,14:6,6:0(0-36),0(0-33):1,1:.,.\n'
+        "##fileformat=VCFv4.2\n",
+        "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\tsample3\n",
+        "chr2\t126162\t.\tREF\t.\t0\t.\tTRID=chr2_126161_126197;END=126197;MOTIFS=CTCTCC;STRUC=(CTCTCC)n\t",
+        "GT:AL:ALLR:SD:MC:MS:AP:AM\t0/0:36,33:35-38,31-33:18,14:6,6:0(0-36),0(0-33):1,1:.,.\n",
     ]
 
-    vcf_lines4 = [
-        '##fileformat=VCFv4.2\n',
-        '#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\tsample4\n',
-        'chr2\t126162\t.\tREF\t.\t0\t.\tTRID=chr2_126161_126197;END=126197;MOTIFS=CTCTCC;STRUC=(CTCTCC)n\tGT:AL:ALLR:SD:MC:MS:AP:AM\t0/0:36,33:35-38,31-33:18,14:6,6:0(0-36),0(0-33):1,1:.,.\n'
-        'chr3\t126162\t.\tREF\t.\t0\t.\tTRID=chr2_126161_126197;END=126197;MOTIFS=CTCTCC;STRUC=(CTCTCC)n\tGT:AL:ALLR:SD:MC:MS:AP:AM\t0/0:36,33:35-38,31-33:18,14:6,6:0(0-36),0(0-33):1,1:.,.\n'
+    out_lines = [
+        "##fileformat=VCFv4.2\n",
+        "##commandline=aggregate_trgt_vcfs.py\n",
+        "CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\tsample1\tsample2\tsample3\n",
+        "chr2\t126162\t.\tREF\tALT1,ALT2,ALT3\t.\t.\tTRID=chr2_126161_126197;END=126197;MOTIFS=CTCTCC;STRUC=(CTCTCC)n\t",
+        "GT:AL:ALLR:SD:MC:MS:AP:AM\t0/1:36,33:35-38,31-33:18,14:6,6:0(0-36),0(0-33):1,1:.,.\t",
+        "2/3:36,33:35-38,31-33:18,14:6,6:0(0-36),0(0-33):1,1:.,.\t0/0:36,33:35-38,31-33:18,14:6,6:0(0-36),0(0-33):1,1:.,.\n",
     ]
 
+    anon_out_lines = [
+        "##fileformat=VCFv4.2\n",
+        "##commandline=aggregate_trgt_vcfs.py\n",
+        "CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\ttest_1\ttest_2\ttest_3\n",
+        "chr2\t126162\t.\tREF\tALT1,ALT2,ALT3\t.\t.\tTRID=chr2_126161_126197;END=126197;MOTIFS=CTCTCC;STRUC=(CTCTCC)n\t",
+        "GT:AL:ALLR:SD:MC:MS:AP:AM\t0/0:36,33:35-38,31-33:18,14:6,6:0(0-36),0(0-33):1,1:.,.\t",
+        "0/1:36,33:35-38,31-33:18,14:6,6:0(0-36),0(0-33):1,1:.,.\t",
+        "2/3:36,33:35-38,31-33:18,14:6,6:0(0-36),0(0-33):1,1:.,.\n",
+    ]
 
     vcf_file1 = get_vcf_file(vcf_lines1)
     vcf_file2 = get_vcf_file(vcf_lines2)
     vcf_file3 = get_vcf_file(vcf_lines3)
-    vcf_file4 = get_vcf_file(vcf_lines4)
 
-    # good check output
     aggregate_trgt_vcfs([vcf_file1, vcf_file2, vcf_file3])
+    out, err = capfd.readouterr()
+    assert out == "".join(out_lines), "printed VCF not as expected"
+
+    random.seed(10)
+    aggregate_trgt_vcfs([vcf_file1, vcf_file2, vcf_file3], anonymize_prefix="test")
+    out, err = capfd.readouterr()
+    assert out == "".join(anon_out_lines), "printed VCF not as expected"
+
+
+def test_different_vcf_lengths():
+    vcf_lines1 = [
+        "##fileformat=VCFv4.2\n",
+        "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\tsample1\n",
+        "chr2\t126162\t.\tREF\tALT1\t0\t.\tTRID=chr2_126161_126197;END=126197;MOTIFS=CTCTCC;STRUC=(CTCTCC)n\t\
+            GT:AL:ALLR:SD:MC:MS:AP:AM\t0/1:36,33:35-38,31-33:18,14:6,6:0(0-36),0(0-33):1,1:.,.\n",
+    ]
+
+    vcf_lines2 = [
+        "##fileformat=VCFv4.2\n",
+        "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\tsample2\n",
+        "chr2\t126162\t.\tREF\t.\t0\t.\tTRID=chr2_126161_126197;END=126197;MOTIFS=CTCTCC;STRUC=(CTCTCC)n\t\
+            GT:AL:ALLR:SD:MC:MS:AP:AM\t0/0:36,33:35-38,31-33:18,14:6,6:0(0-36),0(0-33):1,1:.,.\n"
+        "chr3\t126162\t.\tREF\t.\t0\t.\tTRID=chr2_126161_126197;END=126197;MOTIFS=CTCTCC;STRUC=(CTCTCC)n\t\
+            GT:AL:ALLR:SD:MC:MS:AP:AM\t0/0:36,33:35-38,31-33:18,14:6,6:0(0-36),0(0-33):1,1:.,.\n",
+    ]
+
+    vcf_file1 = get_vcf_file(vcf_lines1)
+    vcf_file2 = get_vcf_file(vcf_lines2)
 
     # first vcf has fewer variants than at least one of the others
     with pytest.raises(IndexError):
-        aggregate_trgt_vcfs([vcf_file1, vcf_file2, vcf_file3, vcf_file4])
+        aggregate_trgt_vcfs([vcf_file1, vcf_file2])
     # first vcf has more variants than at least one of the others
     with pytest.raises(IndexError):
-        aggregate_trgt_vcfs([vcf_file4,vcf_file1, vcf_file2, vcf_file3])
+        aggregate_trgt_vcfs([vcf_file2, vcf_file1])
 
 
 def run_all_test_functions(mod):
@@ -85,8 +124,8 @@ def run_all_test_functions(mod):
             value()
 
 
+if __name__ == "__main__":
+    """This is executed when run from the command line"""
 
-if __name__ == '__main__':
-    """ This is executed when run from the command line """
-
+    # this is not exectuted by pytest, it's for development purposes
     run_all_test_functions(sys.modules[__name__])
